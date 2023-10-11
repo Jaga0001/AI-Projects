a19e01f7dfd8b2a618301bbeec3992ba19a6a177
Jagavantha PA <144882407+Jaga0001@users.noreply.github.com>
2023-10-11 22:36:28 +0530

    Delete Day - 4 directory

--- Day - 4/Ex 2 - Gaussian Blur.py
+++
@@ -1,4 +0,0 @@
-import cv2 
-img = cv2.imread("C://Users/jagaj/Desktop/Jaga/Artificial Intelligence/Day - 4/Anime.jpg")
-gaussianBlurImg = cv2.GaussianBlur(img, (21, 21), 0)
-cv2.imwrite("Gaussian Blur Image.jpg", gaussianBlurImg)
